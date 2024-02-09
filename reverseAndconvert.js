a52d00a2df3c3c3e5571ef1d871132da19603ff8
DheeruBhardwaj <41050840+DheeruBhardwaj@users.noreply.github.com>
2024-02-09 23:17:32 +0530

    Save reverseAndconvert.js.

---
+++ reverseAndconvert.js
@@ -0,0 +1,10 @@
+let fdate= "what's your perfect first date".split("")
+    .map(parseInt)
+    .filter((a)=>a)
+    .reduce((a, b)=> a+b).toString()
+    .split("")
+    .reverse()
+    .join();
+    
+    console.log(fdate);
+
