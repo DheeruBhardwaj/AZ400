64eba7d5d1d887a075a6da0a20c3cab90dee48ee
DheeruBhardwaj <41050840+DheeruBhardwaj@users.noreply.github.com>
2024-02-09 23:19:31 +0530

    Save reverseAndconvert.js.

--- reverseAndconvert.js
+++ reverseAndconvert.js
@@ -1,10 +1,9 @@
-let firstDate= "what's your perfect first date".split("")
-    .map(parseInt)
-    .filter((a)=>a)
-    .reduce((a, b)=> a+b).toString()
-    .split("")
-    .reverse()
-    .join();
-    
-    console.log(firstDate);
+let firstDate = "what's your perfect first date".split("")
+.map(parseInt)
+.filter((a)=>a)
+.reduce((a, b)=> a+b).toString()
+.split("")
+.reverse()
+.join();
 
+console.log(firstDate);
